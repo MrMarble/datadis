3e8d611afe186470ce23780874ef391344809f19
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-08-28 02:04:11 +0200

    feat: get token function

--- datadis/__init__.py
+++ datadis/__init__.py
@@ -1,1 +1,16 @@
-__version__ = '0.1.0'
+import requests
+
+_HOST = 'https://datadis.es'
+_ENDPOINTS = {
+    'get_token': f'{_HOST}/nikola-auth/tokens/login',
+    'get_supplies': f'{_HOST}/api-private/api/get-supplies'
+}
+
+
+def get_token(username: str, password: str) -> str:
+    credentials = {'username': username, 'password': password}
+    r = requests.post(_ENDPOINTS['get_token'], data=credentials)
+    if r.status_code == 200:
+        return r.text
+    else:
+        raise Exception(f'Error: {r.json()["message"]}')
--- tests/test_datadis.py
+++ tests/test_datadis.py
@@ -1,5 +1,26 @@
-from datadis import __version__
+from datadis import get_token
+from unittest import mock
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def mock_requests(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, text_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.text_data = text_data
+
+        def json(self):
+            return self.json_data
+
+        @property
+        def text(self):
+            return self.text_data
+
+    return MockResponse([{"cups": "of rice"}], 'token_ok', 200)
+
+
+@mock.patch('requests.post', side_effect=mock_requests)
+def test_get_token(mock_post: mock.MagicMock):
+    token = get_token("username", "password")
+    assert token is not None
+    assert token == 'token_ok'
